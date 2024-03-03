e045566aec3ad1a8a4d97441bdc538a585b76d1c
hugohdezm03 <alu0101481227@ull.edu.es>
2024-03-03 20:51:34 +0000

    Patrón Proxy

---
+++ src/proxy.ts
@@ -0,0 +1,94 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * 
+  * Design Patterns Presentation.
+  * Programación de Interfaces Interactivas
+  * 
+  * @file adapter.ts
+  * @author Hugo Hernández Martín (alu0101481227)
+  * @author Esther Medina Quintero (alu0101434780)
+  * @date 03/03/2024
+  * @brief Explicative implementation of adapter pattern.
+  * Facade is a structural design pattern.
+  * @see {@link https://refactoring.guru/design-patterns/proxy}
+  * @see {@link https://www.patterns.dev/vanilla/proxy-pattern/}
+  */
+
+/**
+ * Defines the common interface for both RealSubject and Proxy so that a
+ * Proxy can be used anywhere a RealSubject is expected.
+ */
+interface PersonInfo {
+  getName(): string;
+  getAge(): number;
+  setName(name: string): void;
+  setAge(age: number): void;
+}
+
+/**
+ * The RealSubject.
+ */
+class Person implements PersonInfo {
+  private name: string = 'Unknown';
+  private age: number = -1;
+  constructor(name?: string, age?: number) {
+    if (!name) this.name = 'Unknown';
+    if (!age) this.age = -1;
+  }
+  // Getters and setters
+  getName(): string { return this.name; }
+  getAge(): number { return this.age; }
+  setName(name: string): void { this.name = name; }
+  setAge(age: number): void { this.age = age; }
+}
+
+class PersonProxy implements PersonInfo {
+  constructor(private person: Person) {}
+  // Getters
+  getName(): string { return this.person.getName(); }
+  getAge(): number { return this.person.getAge(); }
+  // Setters with validation
+  setName(name: string): void {
+    if (/[A-Z][a-z]+/.test(name)) {
+      this.person.setName(name);
+    } else {
+      throw new Error('Invalid name');
+    }
+  }
+  setAge(age: number): void {
+    if (!Number.isInteger(age) || age < 0) {
+      throw new Error('Invalid age');
+    }
+    this.person.setAge(age);
+  }
+}
+
+function mainProxy() {
+  const proxy = new PersonProxy(new Person());
+  console.log('Before set properties:');
+  console.log(proxy.getName()); // Unkwon
+  console.log(proxy.getAge()); // -1
+
+  proxy.setName('Hugo');
+  proxy.setAge(20);
+  console.log('After set properties:')
+  console.log(proxy.getName()); // Hugo
+  console.log(proxy.getAge()); // 20
+
+  try {
+    proxy.setName('esther'); // Error: Invalid name
+  } catch(error: any) {
+    console.error('Error:', error.message);
+  }
+  try {
+    proxy.setAge(-1); // Error: Invalid age
+  } catch(error: any) {
+    console.error('Error:', error.message);
+  }
+}
+
+if (require.main === module) {
+  mainProxy();
+}
