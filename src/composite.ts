a85ceb76cb7f1249ca4ec27bdd9cc74db9dab370
EstherMedinaQuintero <alu0101434780@ull.edu.es>
2024-03-04 00:55:03 +0000

    Ejemplo de composite

--- src/composite.ts
+++ src/composite.ts
@@ -13,5 +13,70 @@
   * @brief Explicative implementation of composite pattern.
   *        Composite is a structural design pattern.
   * @see {@link https://refactoring.guru/design-patterns/composite}
-  * @see {@link https://www.geeksforgeeks.org/adapter-pattern/}
+  * @see {@link https://www.netguru.com/blog/top-5-most-used-patterns-in-oop-with-typescript}
   */
+
+
+interface Component {
+  operation: Function;
+}
+
+abstract class Leaf implements Component {
+  operation() {}
+}
+
+abstract class Composite implements Component {
+  protected childs: Component[] = [];
+  operation() {
+    this.childs.forEach(child => {
+      child.operation();
+    });
+  }
+  add(component: Component) {
+    this.childs.push(component);
+  }
+  remove(component: Component) {
+    this.childs.splice(this.childs.indexOf(component), 1);
+  }
+  getChild() {
+    return this.childs;
+  }
+}
+
+class Duck extends Composite {
+  constructor(childs: Component[]) {
+    super();
+    this.childs = childs;
+  }
+}
+
+class DuckVoice extends Leaf {
+  operation() {
+    console.log('Quack.');
+  }
+}
+
+class DuckFly extends Composite {
+  operation() {
+    console.log('It flies.');
+    super.operation();
+  }
+  add(component: Component) {
+    super.add(component);
+    return this;
+  }
+}
+
+class Wing extends Leaf {
+  operation() {
+    console.log('Flap-flap-flap');
+  }
+}
+
+const wings = [new Wing(), new Wing()];
+const flyAbility = new DuckFly().add(wings[0]).add(wings[1]);
+const voiceAbility = new DuckVoice();
+
+const duck = new Duck([flyAbility, voiceAbility]);
+
+duck.operation();
